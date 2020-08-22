9072d202ae74f01bdcb88766521db8b8b186acb7
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-22 16:35:20 +0100

    add calculateOrder function and test it

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from customer import Customer
 from order import Order
+from orderDetails import OrderDetails
 
-c = Customer("hafid","id-baha")
-print(c)
-
-o = Order(111)
-o.addItem('tid')
-o.addItem('javel')
-o.showItems()
+od = OrderDetails()
+OrderDetails.calculateOrderPrice()
--- orderDetails.py
+++ orderDetails.py
@@ -3,5 +3,9 @@ class OrderDetails:
     def __init__(customer,order):
         self.customer = customer
         self.order = order
+        self.orderPrice = 0
 
-    
+    def calculateOrderPrice():
+        for item in self.order.items:
+            self.orderPrice += int(item.split(sep=':')[1])
+        
