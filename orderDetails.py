df3aeff34861cf283989ce8c3e5b4c5dcacdbd7c
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-22 16:44:56 +0100

    add funtion to appl the order to the customer and recalculate customer credit

--- orderDetails.py
+++ orderDetails.py
@@ -4,11 +4,14 @@ class OrderDetails:
         self.customer = customer
         self.order = order
         self.orderPrice = 0
+        calculateOrderPrice()
 
     def calculateOrderPrice(self):
         for item in self.order.items:
             self.orderPrice += int(item.split(sep=':')[1])
 
+    def applyOrder(self):
+        self.customer.checkout(self.orderPrice)
 
     def __str__(self):
         print(self.customer.fname+" "+self.customer.lname)     
