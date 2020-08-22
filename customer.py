c22057720e947b666fa7f1e8a30c415f34ac3706
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-22 16:16:05 +0100

    adding  new field to calculate customer credit

--- customer.py
+++ customer.py
@@ -1,8 +1,9 @@
 class Customer:
 
-    def __init__(self,fname,lname):
+    def __init__(self,fname,lname,credit):
         self.fname = fname
         self.lname = lname
+        self.credit = credit
 
     def __str__(self):
         return self.fname+" "+self.lname
