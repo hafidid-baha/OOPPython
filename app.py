fef737536f6d5a55da4230825f05aa9cf0d408ef
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-22 15:55:03 +0100

    adding app file as an entry point to the application

---
+++ app.py
@@ -0,0 +1,4 @@
+from customer import Customer
+
+c = Customer("hafid","id-baha")
+print(c)
--- customer.py
+++ customer.py
@@ -1,8 +1,8 @@
 class Customer:
 
-    __init__(self,fname,lname):
+    def __init__(self,fname,lname):
         self.fname = fname
         self.lname = lname
 
-    __str__(self):
+    def __str__(self):
         return self.fname+" "+self.lname
